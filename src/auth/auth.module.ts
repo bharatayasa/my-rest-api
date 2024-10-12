6676786ecdb0600bb5a5881c35f4d7329f5718b8
bharatayasa <bharatayasa40@gmail.com>
2024-10-12 23:26:32 +0800

    e2e test

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,14 @@
+import { PrismaModule } from './../../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaModule } from 'prisma/prisma.module';
+
 
 @Module({
   imports: [
     PrismaModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,60 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { AuthService } from '../src/auth/auth.service';
 
-describe('AppController (e2e)', () => {
+describe('AuthController (e2e)', () => {
   let app: INestApplication;
+  let authService: AuthService;
+  let prismaService: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    authService = moduleFixture.get<AuthService>(AuthService);
+    prismaService = moduleFixture.get<PrismaService>(PrismaService);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('POST /auth/login', () => {
+    it('should return access token and success message', async () => {
+      const validUser = {
+        email: 'bhar@example.commmmm1',
+        password: '123',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(validUser)
+        .expect(200);
+
+      expect(response.body).toHaveProperty('message', 'Login successful');
+      expect(response.body.data).toHaveProperty('token');
+    });
+
+    it('should return Invalid email or password', async () => {
+      const invalidUser = {
+        email: 'invalid@example.com',
+        password: 'wrongpassword',
+      };
+
+      const response = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send(invalidUser)
+        .expect(401);
+
+      expect(response.body).toHaveProperty('message', 'Login failed');
+      expect(response.body).toHaveProperty('error');
+    });
   });
 });
